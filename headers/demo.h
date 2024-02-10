f10d7c498fc044f41ae5fe3efed74f7b9bc88230
fk2019 <fkmuiruri8@gmail.com>
2024-02-10 14:22:57 +0000

    Add project files

---
+++ headers/demo.h
@@ -0,0 +1,11 @@
+#ifndef _DEMO_H_
+#define _DEMO_H_
+#include <SDL2/SDL.h>
+
+typedef struct SDL_Instance
+{
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+} SDL_Instance;
+int init_instance(SDL_Instance *);
+#endif /*_DEMO__ */
